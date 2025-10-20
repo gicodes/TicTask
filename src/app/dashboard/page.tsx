2e9ab34446ab9c13ff66c8e9e0cc1f67e1427807
Gideon I <gicodes9@gmail.com>
2025-10-20 01:45:45 +0100

    Major Bug fix: Solved nextAuth API_URL, env variables and authOptions signIn congig, Added user.name and user.photo to next-auth/jwt

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useAuth } from '@/providers/auth';
+import { useEffect, useState } from 'react';
 import TicketsBoardPage from './_level_3/ticket';
 import { Box, Divider, Stack, Typography } from '@mui/material';
 
@@ -11,6 +12,16 @@ export default function Page() {
 
   if (!isAuthenticated) 
     return <Box textAlign={'center'} p={4}> Please log in to access dashboard </Box>;
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Box py={1}>
@@ -26,7 +37,7 @@ export default function Page() {
         <Typography variant="h5" fontWeight={501}>
           Welcome, {user?.name}
         </Typography>
-        <Typography variant='subtitle2'>{new Date().toLocaleString()}</Typography>
+        <Typography variant='subtitle2'>{time.toLocaleString()}</Typography>
       </Stack>
       
       <Divider />
