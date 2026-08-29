9b34c4cce6c5bb68d3fe1fc8ef48b1aaea5091ef
Gideon I <gicodes9@gmail.com>
2026-08-29 14:11:39 +0100

    removed authRedirect from sessionWatcher to undo useSearchParams error

--- src/lib/sessionWatcher.ts
+++ src/lib/sessionWatcher.ts
@@ -2,17 +2,14 @@
 
 import { useEffect } from "react";
 import { useSession, signOut } from "next-auth/react";
-import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 
 export function SessionErrorWatcher() {
   const { data: session } = useSession();
-  const { getLoginUrl } = useAuthRedirect();
-  const callbackUrl = getLoginUrl();
 
   useEffect(() => {
     if (session?.error === "RefreshAccessTokenError") {
-      if (process.env.NODE_ENV !== "production") console.error("Signed out with callbackUrl:", callbackUrl);
-      // signOut({ callbackUrl });
+      if (process.env.NODE_ENV !== "production") console.error("Signed out with callbackUrl:");
+      // signOut({ callbackUrl }); x
     }
   }, [session?.error]);
 
